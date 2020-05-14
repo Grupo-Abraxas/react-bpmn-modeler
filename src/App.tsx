6efc01989f3f8b30edda00b22081e54c8482f46c
Javiermolinah <ing.molina.h@gmail.com>
2020-05-14 15:34:18 -0500

    Add test alert function

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Bpmn from './components/Bpmn'
 
 const App = () => <Bpmn
-  onTaskTarget={(e: CustomEvent) => console.log(e.detail)}
+  onTaskTarget={(e: CustomEvent) => alert(e.detail)}
   onError={(error: string) => alert(error)}
 />
 
